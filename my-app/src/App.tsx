f0415da3bdb21b1ef66a17b472c40509ae4120e9
Isaac Blanco <nuclearhands2001@gmail.com>
2022-10-03 16:05:31 +0000

    Update 7 files

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,12 @@
-import React from 'react';
+import React from "react";
+import PaymentOption from "./PaymentOptions";
+import PaymentValue from "./PaymentValue";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PaymentOption></PaymentOption>
+      <PaymentValue></PaymentValue>
     </div>
   );
 }
