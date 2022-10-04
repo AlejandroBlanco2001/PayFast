e53c30ac782af0821b1587214e5a0d9842529ddf
unknown <germandavidvargas@hotmail.com>
2022-10-04 09:27:16 -0500

    Services and docker bases created

---
+++ services/queries-api/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  return console.log(`Express is listening at http://localhost:${process.env.PORT}`);
+});
--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const app = express();
-const port = 3000;
-
-
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
