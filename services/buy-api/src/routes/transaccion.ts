2fdd9922bd4d4cb865da16a46e497c467f7996e7
unknown <germandavidvargas@hotmail.com>
2022-11-18 11:56:43 -0500

    transaccion get methods corrected

--- services/buy-api/src/routes/transaccion.ts
+++ services/buy-api/src/routes/transaccion.ts
@@ -1,9 +1,11 @@
 import {Router } from 'express';
-import {createTransaccion} from '../controllers/transaccion';
-import { verifyTransaccion } from '../utils/jwt';
+import {createTransaccion, getTransaccionbyUser, getTransacciones} from '../controllers/transaccion';
+import { verifyTransaccion, verifyAdmin } from '../utils/jwt';
 
 const router = Router();
 
 router.post('/', verifyTransaccion, createTransaccion);
+router.get('/user/:id', verifyTransaccion, getTransaccionbyUser);
+router.get('/', verifyAdmin, getTransacciones);
 
 export default router;
