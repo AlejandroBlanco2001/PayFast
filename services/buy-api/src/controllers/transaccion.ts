86f391c7299e56d267d64e545a7e3f1b2c18f578
Isaac Blanco <nuclearhands2001@gmail.com>
2022-11-21 15:19:49 -0500

    Finishing all

--- services/buy-api/src/controllers/transaccion.ts
+++ services/buy-api/src/controllers/transaccion.ts
@@ -9,7 +9,6 @@ enum Estado {
 }
 
 export const createTransaccion = async (req: express.Request, res: express.Response, next:express.NextFunction) => {
-    console.log(req.app.get('queue'));
     //Monto mayor a 0
     if(req.body.monto <= 0) {
         return res.status(400).json({message: "Monto must be greater than 0"});
@@ -25,7 +24,7 @@ export const createTransaccion = async (req: express.Request, res: express.Respo
             },
         });
         if (!metodo.estado) {
-            return res.status(404).json({message: "Metodo not available"});
+            estado = Estado.Rechazado;
         }
 
         //verificar estado de servicio de transaccion
@@ -55,19 +54,22 @@ export const createTransaccion = async (req: express.Request, res: express.Respo
                 estado: estado
             },
         });
-        res.status(201).json({transaccion});
-    } catch (err) {
-        if (err.status === 500) {
-            req.app.get('queue').push({
-                monto: req.body.monto,
-                sede: req.body.sede,
-                franquicia: req.body.franquicia,
-                nroCuotas: req.body.nroCuotas,
-                userId: req.body.userid,
-                metodoId: req.body.metodoId,
-                estado: estado
+
+        if(estado === Estado.Aprobado){
+            //actualizar saldo
+            const saldo = metodo.saldo - req.body.monto;
+            await prisma.metodopago.update({
+                where: {
+                    id: req.body.metodoId,
+                },
+                data: {
+                    saldo: saldo,
+                },
             });
         }
+        res.status(201).json({transaccion});
+    } catch (err) {
+        next(err)
     }
 };
 
--- services/queries-api/src/controllers/metodos.ts
+++ services/queries-api/src/controllers/metodos.ts
@@ -77,6 +77,8 @@ export const createMetodo = async (req: express.Request, res: express.Response, 
                 userId: req['user'].id,
                 bancoId: req.body.bancoId,
                 tipo: req.body.tipo,
+                numero: req.body.numero,
+                CVC: req.body.cvc,
             },
         });
         res.status(201).json({metodo});
@@ -98,6 +100,8 @@ export const updateMetodo = async (req: express.Request, res: express.Response, 
                 userId: req.body.userId || undefined,
                 bancoId: req.body.bancoId || undefined,
                 tipo: req.body.tipo || undefined,
+                numero: req.body.numero || undefined,
+                CVC: req.body.cvc || undefined,
             },
         });
         res.status(200).json({metodo});
