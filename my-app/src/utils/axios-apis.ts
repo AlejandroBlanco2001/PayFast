362ef8ed97ad9e47d2ab4182bb214fa15214bcf9
LeoLizc <ldlizcano@uninorte.edu.co>
2022-11-21 16:24:58 -0500

    id middleware

--- my-app/src/utils/axios-apis.ts
+++ my-app/src/utils/axios-apis.ts
@@ -1,4 +1,19 @@
-import axios from 'axios';
+import axios, { Axios, AxiosRequestConfig } from 'axios';
+
+// add an interceptor to handle the request and add the user to the body of the request
+
+const addUserId = (config: AxiosRequestConfig ) => {
+    const user = localStorage.getItem('user');
+    if (user) {
+        
+        if(config.method === 'post' || config.method === 'put') {
+            config.data = {...config.data, id: user};
+        } else if(config.method === 'get') {
+            config.params = {...config.params, id:user};
+        }
+    }
+    return config;
+};
 const buy_api = axios.create({
     withCredentials: true,
     baseURL: process.env.BUY_URL || 'http://localhost:5000',
@@ -11,6 +26,11 @@ const security_api = axios.create({
     withCredentials: true,
     baseURL: process.env.SECURITY_URL || 'http://localhost:8000',
 });
+
+queries_api.interceptors.request.use(addUserId);
+security_api.interceptors.request.use(addUserId);
+buy_api.interceptors.request.use(addUserId);
+
 export {
     buy_api,
     queries_api,
